7adb6d6ea7e6ef139f48e89e71043098f15ef541
Diniar90 <arjonadiego90@gmail.com>
2024-01-22 20:37:52 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) );
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+    console.log('Servidor corriendo en puerto: '+port);
+});
+
+app.get('/', (req, res) => {
+    let htmlpath = path.resolve(__dirname, './views/index.html');
+    res.sendFile(htmlpath);
+});
+
+
